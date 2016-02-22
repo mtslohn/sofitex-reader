5d4a0f724d272f13453b8a672e9fd22f777436ef
Mateus Lohn Andriani <mtslohn@gmail.com>
2016-02-21 22:58:56 -0300

    Initial commit.

---
+++ src/main/java/br/ufsc/egc/sofitexreader/App.java
@@ -0,0 +1,13 @@
+package br.ufsc.egc.sofitexreader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
