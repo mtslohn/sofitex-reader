16c0d84f9f86dfb6978154b87bf2696df1f780de
Mateus Lohn Andriani <mtslohn@gmail.com>
2016-02-21 23:59:35 -0300

    Início da implementação da leitura do índice da SOFITEX.

--- src/main/java/br/ufsc/egc/sofitexreader/App.java
+++ src/main/java/br/ufsc/egc/sofitexreader/App.java
@@ -1,13 +1,26 @@
 package br.ufsc.egc.sofitexreader;
 
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.search.Collector;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.MatchAllDocsQuery;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	
+	public static void main(String[] args) {
+		
+		Object directoryReader = DirectoryReader.open(FSDirectory.open("index/pessoa"));
+		IndexSearcher searcher = new IndexSearcher(directoryReader);
+		
+		TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), Integer.MAX_VALUE);
+		
+	}
+	
 }
