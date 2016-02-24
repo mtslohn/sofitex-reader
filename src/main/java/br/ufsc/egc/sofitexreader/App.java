511571bfb493cb10bec64e07a95fa6351e608b07
Mateus Lohn Andriani <mtslohn@gmail.com>
2016-02-23 23:22:47 -0300

    Adicionado JAR de dependências para carga de versões anteriores do Lucene.

--- src/main/java/br/ufsc/egc/sofitexreader/App.java
+++ src/main/java/br/ufsc/egc/sofitexreader/App.java
@@ -1,8 +1,9 @@
 package br.ufsc.egc.sofitexreader;
 
+import java.io.IOException;
+import java.nio.file.FileSystems;
+
 import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.search.Collector;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.MatchAllDocsQuery;
 import org.apache.lucene.search.TopDocs;
@@ -16,11 +17,20 @@ public class App {
 	
 	public static void main(String[] args) {
 		
-		Object directoryReader = DirectoryReader.open(FSDirectory.open("index/pessoa"));
-		IndexSearcher searcher = new IndexSearcher(directoryReader);
-		
-		TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), Integer.MAX_VALUE);
-		
+		try {
+			
+			FSDirectory directory = FSDirectory.open(FileSystems.getDefault().getPath("index/pessoa"));
+			DirectoryReader directoryReader = DirectoryReader.open(directory);
+			IndexSearcher searcher = new IndexSearcher(directoryReader);
+			
+			TopDocs topDocs = searcher.search(new MatchAllDocsQuery(), Integer.MAX_VALUE);
+			
+			System.out.println(topDocs.totalHits);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
